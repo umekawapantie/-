d2cf3037ed0ea34a946e5770f9daee525be80237
Jane54542 <65839357+Jane54542@users.noreply.github.com>
2020-06-11 15:16:11 +0800

    Update Main_Menu.c

--- UI/Main_Menu.c
+++ UI/Main_Menu.c
@@ -1,65 +1,98 @@
-/*
-* Copyright(C), 2007-2008, XUPT Univ.	 
-* File name: Main_Menu.c			 	 
-* Description : TTMS 系统主菜单	
-* Author:   XUPT  		 
-* Version:  v.1 	 
-* Da
-*/
-
-#include "../View/Main_Menu.h"
-
-#include <stdio.h>
-
-#include "../View/Studio_UI.h"
-
-//系统主菜单 
+#include<stdio.h>
+#include<stdlib.h>
+#include"./Account_UI.h"
+#include"./Account_Srv.h"
+#include"./List.h"
 void Main_Menu(void) {
 	char choice;
-	do { 
-		printf("\n==================================================================\n");
-		printf("**************** Theater Ticket Management System ****************\n");
-		printf("[S]tudio Management.\n");
-		printf("[P]lay Management.\n");
-		printf("[T]icket Sale.\n");
-		printf("[R]eturn Ticket.\n");
-		printf("[Q]ueries\n");
-		printf("Ra[n]king and Statistics.\n");
-		printf("[A]ccount Management.\n");
-		printf("[E]xist.\n");
-		printf("\n==================================================================\n");
-		printf("Please input your choice:");
-		fflush(stdin);
-		choice = getchar();
-		switch (choice) {
-		case 'S':
-		case 's':
-			Studio_UI_MgtEntry();
-			break;
-		case 'P':
-		case 'p':
-			Play_UI_MgtEntry(0);
-			break;
-		case 'Q':
-		case 'q':
-			Queries_Menu();
-			break;
-		case 'T':
-		case 't':
-			Sale_UI_MgtEntry();
-			break;
-		case 'R':
-		case 'r':
-			Sale_UI_ReturnTicket();
-			break;
-		case 'N':
-		case 'n':
-			SalesAanalysis_UI_MgtEntry();
-			break;
-		case 'A':
-		case 'a':
-			Account_UI_MgtEntry();
-			break;
-		}
-	} while ('E' != choice && 'e' != choice);
+	int ch, m, pass = 0;//登录
+	printf("\t\t\t\t======================welcome to LLMZ ciname==================\n\n");
+	printf("\t\t\t\tDo you have account?                    Yes[1] / No[0]\n\n");
+	printf("\t\t\t\t==============================================================\n");
+	printf("\t\t\t\t");
+	printf("\t\t\t\tPlease input your choice:");
+	scanf("%d", &ch);
+	account_list_t head;
+	account_node_t* temp;
+	List_Init(head, account_node_t);
+	if (!ch)
+	{		
+		Account_UI_Add(head);
+	}
+	if (Login()) pass = 1;
+	if (pass)//登陆通过执行功能
+	{
+		printf("Please input your username again:");
+		scanf("%s",temp->data.username);
+		temp=Account_FindByUserName(head,temp->data.username);
+		do {
+			printf("\t\t\t\t=======================welcome to LLMZ ciname=====================\n\n");
+			printf("\t\t\t\t [S]Studio Management.\t\t\t");
+			printf("[P]Play Management.\n\n");
+			printf("\t\t\t\t [T]Ticket Sale.\t\t\t");
+			printf("[R]Return Ticket.\n\n");
+			printf("\t\t\t\t [Q]Queries\t\t\t\t");
+			printf("[N]Ranking and Statistics.\n\n");
+			printf("\t\t\t\t [A]Account Management.\t\t\t");
+			printf("[I] Personal information.\n\n");
+			printf("\t\t\t\t [E]Exist.\n\n");
+			printf("\t\t\t\t===================================================================\n");
+			printf("Please input your choice:");
+			fflush(stdin);
+			choice = getchar();
+			switch (choice)
+			{
+			case 'S':
+			case 's':
+				if (temp->data.type == 9)
+					Studio_UI_MgtEntry();//影院管理
+				else printf("Sorry!You don't have access!");
+				break;
+			case 'P':
+			case 'p':
+				if(temp->data.type==2)
+				    Play_UI_MgtEntry();//演出安排
+				else printf("Sorry!You don't have access!");
+				break;
+			case 'Q':
+			case 'q':
+				if (temp->data.type == 1||temp->data.type==2)
+				   Queries_Menu();//查询票务信息
+				break;
+			case 'T':
+			case 't':
+				if(temp->data.type==1)
+				    Sale_UI_MgtEntry();//售票管理
+				else printf("Sorry!You don't have access!");
+				break;
+			case 'R':
+			case 'r':
+				if (temp->data.type == 1)
+				    Sale_UI_ReturnTicket();//退票管理
+				else printf("Sorry!You don't have access!");
+				break;
+			case 'N':
+			case 'n':
+				if (temp->data.type == 1||temp->data.type==2)
+				   SalesAanalysis_UI_MgtEntry();//统计销售额
+				else printf("Sorry!You don't have access!");
+				break;
+			case 'A':
+			case 'a':
+				if(temp->data.type==9)
+				    Account_UI_MgtEntry();//系统用户管理
+				else printf("Sorry!You don't have access!");
+				break;
+			case 'I':
+			case 'i':
+				MaiAccount_UI_MgtEntry();//维护个人信息
+			}
+		} while ('E' != choice && 'e' != choice);
+	}
+	else
+	{
+		printf("Sorry!You don't have access!");
+	}
 }
+
+
